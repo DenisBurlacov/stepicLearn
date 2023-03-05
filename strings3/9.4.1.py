45f29ae693e6b9bac29431f54a5914d056f106d5
Denis Burlakov <denis055023@gmail.com>
2023-03-05 15:55:29 +0100

    last

---
+++ strings3/9.4.1.py
@@ -0,0 +1,8 @@
+# s = "I learn Python language. Python - awesome!"
+# print(s.find("Python"))
+
+# s = '     I learn Python language               '
+# print(s.strip())
+
+s = 'abcdababa'
+print(s.replace('ab', '123'))
